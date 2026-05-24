eaa9b864a0749c7d00af95a90eaf096fe9679103
sheyangong-lab <sxs1827155255@gmail.com>
2026-05-24 19:57:22 +0800

    Delete create-aeronautics-nocopy directory

--- create-aeronautics-nocopy/src/main/java/com/sablepatch/nbtguard/SableNbtGuard.java
+++
@@ -1,9 +0,0 @@
-package com.sablepatch.nbtguard;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class SableNbtGuard {
-    public static final String MOD_ID = "sablenbtguard";
-    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-}
